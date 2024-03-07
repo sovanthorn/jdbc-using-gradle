1f538d1bc96960d6674bcf471a47c6973a74d48a
Keo  Kay <keokay888@gmail.com>
2024-03-07 14:20:47 +0700

    update the repsitory

--- src/main/java/repository/PersonRepository.java
+++ src/main/java/repository/PersonRepository.java
@@ -1,6 +1,7 @@
 package repository;
 
 import com.github.javafaker.Faker;
+import lombok.extern.slf4j.Slf4j;
 import model.Person;
 import utils.PropertyUtils;
 import utils.SQLUtils;
@@ -10,40 +11,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
+
+@Slf4j
 public class PersonRepository {
 
     private final Properties properties;
-    public PersonRepository(){
+
+    public PersonRepository() {
         properties = PropertyUtils.loadProperty();
     }
 
-    private Connection startDatabaseConnection() throws SQLException{
-       return  DriverManager.getConnection(
+    private Connection startDatabaseConnection() throws SQLException {
+        return DriverManager.getConnection(
                 properties.getProperty("DB_URL"),
                 properties.getProperty("USERNAME"),
                 properties.getProperty("PASSWORD")
         );
     }
-    public List<Person> getAllPerson(){
-        try(
+
+    public List<Person> getAllPerson() {
+        try (
                 Connection connection = startDatabaseConnection();
                 Statement statement = connection.createStatement();
-                ){
-                var personList = new ArrayList<Person>();
-                var rs = statement.executeQuery(SQLUtils.PersonSQL.getAllPersonSql);
-                while(rs.next()){
-                    personList.add(
-                            new Person()
-                                    .setId(rs.getInt("id"))
-                                    .setFullName(rs.getString("fullname"))
-                                    .setEmail(rs.getString("email"))
-                                    .setAddress(rs.getString("address"))
-                                    .setGender(rs.getString("gender"))
-                            );
-                }
-                return personList;
+        ) {
+            var personList = new ArrayList<Person>();
+            var rs = statement.executeQuery(SQLUtils.PersonSQL.getAllPersonSql);
+            while (rs.next()) {
+                personList.add(
+                        new Person()
+                                .setId(rs.getInt("id"))
+                                .setFullName(rs.getString("fullname"))
+                                .setEmail(rs.getString("email"))
+                                .setAddress(rs.getString("address"))
+                                .setGender(rs.getString("gender"))
+                );
+            }
+            return personList;
 
-        }catch (SQLException ex ){
+        } catch (SQLException ex) {
             System.out.println("Failed to retreive all the person data ! ");
             ex.printStackTrace();
         }
@@ -51,21 +56,63 @@ public class PersonRepository {
         return null;
     }
 
-    public int addNewPerson(Person person){
-        try(
+    public int addNewPerson(Person person) {
+        try (
                 Connection connection = startDatabaseConnection();
                 PreparedStatement ps = connection.prepareStatement(SQLUtils.PersonSQL.insertNewPerson);
-                ){
-            ps.setString(1,person.getFullName());
-            ps.setString(2,person.getGender());
-            ps.setString(3,person.getEmail());
-            ps.setString(4,person.getAddress());
+        ) {
+            ps.setString(1, person.getFullName());
+            ps.setString(2, person.getGender());
+            ps.setString(3, person.getEmail());
+            ps.setString(4, person.getAddress());
             return ps.executeUpdate();
 
-        }catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Error when adding a new person");
             ex.printStackTrace();
         }
         return 0;
     }
+
+    public int updatePerson(Person updatedPerson) {
+        try
+                (
+                        Connection connection = startDatabaseConnection();
+                        PreparedStatement ps = connection.prepareStatement(SQLUtils.PersonSQL.updatePerson)
+                ) {
+
+            ps.setString(1,updatedPerson.getFullName());
+            ps.setString(2,updatedPerson.getGender());
+            ps.setString(3,updatedPerson.getEmail());
+            ps.setString(4,updatedPerson.getAddress());
+            ps.setInt(5,updatedPerson.getId());
+
+            return ps.executeUpdate();
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return 0;
+        }
+
+    }
+
+    public int deletePersonByID(int personID) {
+        try (
+                Connection connection = startDatabaseConnection();
+                PreparedStatement ps = connection.prepareStatement(SQLUtils.PersonSQL.deletePersonById)
+
+        ) {
+            ps.setInt(1, personID);
+            return ps.executeUpdate(); // return int -> number of records that we deleted !
+
+        } catch (SQLException ex) {
+            System.out.println("Failed to delete the person record with ID = " + personID);
+            ex.printStackTrace();
+            return 0;
+        }
+
+    }
+
+
 }
--- src/main/java/utils/SQLUtils.java
+++ src/main/java/utils/SQLUtils.java
@@ -2,12 +2,25 @@ package utils;
 
 public class SQLUtils {
     public  static class PersonSQL{
+
+        // all constant must in uppercase
         public static final String getAllPersonSql= """
             select * from person_tb;
             """;
-
         public static final String insertNewPerson= """
                 INSERT INTO person_tb ("fullname","gender","email","address") VALUES(?,?,?,?);
                 """;
+
+        public  static final String deletePersonById = """
+                delete from person_tb where id = ?
+                """;
+
+        public  static final String updatePerson= """
+                update person_tb set  fullname=?,gender=?,email=?,address=?
+                where id = ?
+                """;
+    }
+    public  static class SystemUser{
+        // sql string in here !
     }
 }
